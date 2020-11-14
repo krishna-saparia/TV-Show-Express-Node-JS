15d9963da09c40632a1bd64d3179d659da966c27
krishna saparia <krishnasaparia43045@gmail.com>
2020-11-14 16:47:00 -0500

    Add episode list on html page

--- public/javascripts/apiData.js
+++ public/javascripts/apiData.js
@@ -1,26 +1,40 @@
 
 async function fetchData(){
-
     var searchData = document.getElementById("txtData").value;
-    apiDetails = await $.ajax(`http://api.tvmaze.com/singlesearch/shows?q= ${searchData}`);
+    var apiDetails1_main = `http://api.tvmaze.com/singlesearch/shows?q= ${searchData}`
+
+    apiDetails = await $.ajax(apiDetails1_main);
     console.log(apiDetails);
-    var showId  = `http://api.tvmaze.com/shows/${apiDetails.id}`;
+   // var showId  = `http://api.tvmaze.com/shows/${apiDetails.id}`;
     var episodelist = await $.ajax(`http://api.tvmaze.com/shows/${apiDetails.id}/episodes`);
     console.log(episodelist);
-    var s = JSON.stringify(episodelist);
-    const jsonObj = JSON.parse(s);
-    for(let i = 0 ; i < jsonObj.length; i++){
-        console.log(jsonObj[i].name);
+    var ep_function = function() {
+        var episode_div = document.getElementById("episodeList");
+        var s = JSON.stringify(episodelist);
+        const jsonObj = JSON.parse(s);
+        for(let i = 1 ; i < jsonObj.length ; i++){
+            var list = document.createElement("li");
+            list.innerHTML = 'Episode '+[i] + ': '  + jsonObj[i].name;
+            episode_div.appendChild(list);
+        }
     }
+       /* var s = JSON.stringify(episodelist);
+        const jsonObj = JSON.parse(s);
+        for(let i = 0 ; i < jsonObj.length; i++){
+            jsonObj[i].name;
+        }*/
+
+
     console.log(`http://api.tvmaze.com/shows/${apiDetails.id}&embed=episodes`);
     if (searchData === apiDetails.name.toLowerCase() || searchData === apiDetails.name.toUpperCase()) {
         showData(apiDetails.name , apiDetails.summary, apiDetails.image.original);
+        ep_function();
     } else {
         alert('The show which are you looking for either not available or check your spelling ');
     }
     /*this.renderResult(showDetails.name);*/
-    var btnC = document.getElementById("btn");
-    if(btnC.onclick === true) this.onclick = null;
+    document.getElementById("btn").addEventListener("click",clearImmediate);
+
 }
 
 
@@ -32,30 +46,27 @@ function showData(name, summary, imgUrl){
 
     // create tags
     header1 = document.createElement('h1');
+    header2 = document.createElement('h2');
     img = document.createElement('img');
     summaryTag = document.createElement('div');
 
-    li = document.createElement('li');
 
 
     // add data
 
     header1.appendChild(document.createTextNode(name));
-
-
-    img.setAttribute('width', '258');
-
+    header1.setAttribute('fontFamily', 'Arial, Helvetica, sans-serif')
+    img.setAttribute('width', '1500');
+    img.setAttribute('height', '800');
     img.setAttribute('src', imgUrl);
-
     summaryTag.innerHTML = summary;
-
+    header2.appendChild(document.createTextNode("Episode List"));
     // add to page
+
     main.appendChild(header1);
-
     main.appendChild(img);
-
     main.innerHTML += summary;
-
+    main.appendChild(header2)
 }
 
 
