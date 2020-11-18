f9b01b9284a69fe076cb1433f623fa1f57798c4e
krishna saparia <krishnasaparia43045@gmail.com>
2020-11-17 23:14:16 -0500

    get season list and it's child (Episode list) according to each season

--- public/javascripts/apiData.js
+++ public/javascripts/apiData.js
@@ -6,34 +6,56 @@ async function fetchData(){
     apiDetails = await $.ajax(apiDetails1_main);
     console.log(apiDetails);
    // var showId  = `http://api.tvmaze.com/shows/${apiDetails.id}`;
+
+    var seasonList = await $.ajax(`http://api.tvmaze.com/shows/${apiDetails.id}/seasons`);
+    console.log(seasonList);
+
     var episodelist = await $.ajax(`http://api.tvmaze.com/shows/${apiDetails.id}/episodes`);
     console.log(episodelist);
     var ep_function = function() {
+        var season_li = document.getElementById("seasonList");
         var episode_div = document.getElementById("episodeList");
+       // header2 = document.createElement('h2');
+
+        var sl = JSON.stringify(seasonList);
+        var jsonSeason = JSON.parse(sl);
+
         var s = JSON.stringify(episodelist);
+
         const jsonObj = JSON.parse(s);
-        for(let i = 1 ; i < jsonObj.length ; i++){
-            var list = document.createElement("li");
-            list.innerHTML = 'Episode '+[i] + ': '  + jsonObj[i].name;
-            episode_div.appendChild(list);
-        }
-    }
-       /* var s = JSON.stringify(episodelist);
-        const jsonObj = JSON.parse(s);
-        for(let i = 0 ; i < jsonObj.length; i++){
-            jsonObj[i].name;
-        }*/
 
 
-    console.log(`http://api.tvmaze.com/shows/${apiDetails.id}&embed=episodes`);
-    if (searchData === apiDetails.name.toLowerCase() || searchData === apiDetails.name.toUpperCase()) {
-        showData(apiDetails.name , apiDetails.summary, apiDetails.image.original);
-        ep_function();
-    } else {
-        alert('The show which are you looking for either not available or check your spelling ');
+
+
+           for (let j = 0; j < jsonSeason.length; j++) {
+                var list = document.createElement("li");
+                list.innerHTML = 'Season ' + [(j+1)];
+                list.setAttribute("id",'Season ' + (j+1));
+                season_li.appendChild(list);
+           }
+            for (let j = 0; j < jsonSeason.length; j++) {
+
+                for (let i = 0; i < jsonObj.length; i++)
+                {
+                    if (jsonObj[i].season === (j+1)  ) {
+                        var list = document.createElement("li");
+                        list.innerHTML = 'Episode '  +jsonObj[i].number+ ': ' + jsonObj[i].name;
+                        document.getElementById('Season ' + (j+1)).appendChild(list);
+                    }
+                }
+            }
     }
-    /*this.renderResult(showDetails.name);*/
-    document.getElementById("btn").addEventListener("click",clearImmediate);
+
+    // if (searchData === apiDetails.name.toLowerCase() || searchData === apiDetails.name.toUpperCase()) {
+    //     showData(apiDetails.name , apiDetails.summary, apiDetails.image.original);
+    //     ep_function();
+    // }
+    // else {
+    //     alert('The show which are you looking for either not available or check your spelling ');
+    // }
+
+    showData(apiDetails.name , apiDetails.summary, apiDetails.image.original);
+    ep_function();
 
 }
 
@@ -46,7 +68,7 @@ function showData(name, summary, imgUrl){
 
     // create tags
     header1 = document.createElement('h1');
-    header2 = document.createElement('h2');
+    //header2 = document.createElement('h2');
     img = document.createElement('img');
     summaryTag = document.createElement('div');
 
@@ -56,17 +78,17 @@ function showData(name, summary, imgUrl){
 
     header1.appendChild(document.createTextNode(name));
     header1.setAttribute('fontFamily', 'Arial, Helvetica, sans-serif')
-    img.setAttribute('width', '1500');
-    img.setAttribute('height', '800');
+    img.setAttribute('width', '340');
+    //img.setAttribute('height', '800');
     img.setAttribute('src', imgUrl);
     summaryTag.innerHTML = summary;
-    header2.appendChild(document.createTextNode("Episode List"));
+    //header2.appendChild(document.createTextNode("Episode List"));
     // add to page
 
     main.appendChild(header1);
     main.appendChild(img);
     main.innerHTML += summary;
-    main.appendChild(header2)
+    //main.appendChild(header2)
 }
 
 
